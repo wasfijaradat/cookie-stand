e00cb22643be9d400f42ecb154b6036f9f0932b2
wasfijaradat <wasfijar@gmail.com>
2021-02-21 23:02:04 +0200

    finalbranch

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict';
-var hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 
-var table = document.getElementById('sales');
+let table = document.getElementById('sales');
 
-var total_stores = [];
+let total_stores = [];
 
-//constructor 
+//constructor for the cookie stores
 function Cookie_store (name ,customer_min , customer_max , average_cookies) {
   this.name = name ,
   this.customer_min = customer_min;
@@ -18,40 +18,40 @@ function Cookie_store (name ,customer_min , customer_max , average_cookies) {
 }
 //creates the numbers of cookies sold for every hour
 Cookie_store.prototype.create_cookie_sales_per_hour = function () {
-  for (var i = 0 ; i < hours.length ; i++) {
-    var sales = customer_per_hour (this.customer_min , this.customer_max ) * Math.floor(this.average_cookies);
+  for (let i = 0 ; i < hours.length ; i++) {
+    let sales = customer_per_hour (this.customer_min , this.customer_max ) * Math.floor(this.average_cookies);
     this.cookie_sales_per_hour.push(sales);
   }
 
 };
 //creates the total of cookies sold in a day
 Cookie_store.prototype.daily_total = function () {
-  for (var i = 0; i < this.cookie_sales_per_hour.length ; i++) {
+  for (let i = 0; i < this.cookie_sales_per_hour.length ; i++) {
     this.cookie_sales_per_day= this.cookie_sales_per_day + this.cookie_sales_per_hour[i];
   }
 };
 
 
-var seattle = new Cookie_store ('Seattle',23,65,6.3);
-var tokyo = new Cookie_store ('Tokyo',3,24,1.2);
-var dubai = new Cookie_store ('Dubai',11,38,3.7);
-var paris = new Cookie_store ('Paris',20,38,2.3);
-var lima = new Cookie_store ('Lima',2,16,4.6);
+let seattle = new Cookie_store ('Seattle',23,65,6.3);
+let tokyo = new Cookie_store ('Tokyo',3,24,1.2);
+let dubai = new Cookie_store ('Dubai',11,38,3.7);
+let paris = new Cookie_store ('Paris',20,38,2.3);
+let lima = new Cookie_store ('Lima',2,16,4.6);
 
 
 // creates header
 function header () {
-  var row = document.createElement('tr');
-  var locations = document.createElement('th');
+  let row = document.createElement('tr');
+  let locations = document.createElement('th');
   locations.textContent = ('Locations');
   row.appendChild(locations);
   //creates hours headers
-  for ( var i = 0 ; i < hours.length ; i++) {
-    var time = document.createElement('th');
+  for ( let i = 0 ; i < hours.length ; i++) {
+    let time = document.createElement('th');
     time.textContent = hours[i];
     row.appendChild(time);
   }
-  var total = document.createElement('th');
+  let total = document.createElement('th');
   total.textContent = ('Daily Totals');
   row.appendChild(total);
 
@@ -62,19 +62,19 @@ header();
 
 // creates the sales body
 function render (store) {
-  var row = document.createElement('tr');
-  var name = document.createElement('th');
+  let row = document.createElement('tr');
+  let name = document.createElement('th');
   name.textContent = store.name;
   row.appendChild(name);
   // creates row for the cookie sales
-  for ( var i = 0; i < hours.length ; i++) {
-    var sale = document.createElement('th');
+  for ( let i = 0; i < hours.length ; i++) {
+    let sale = document.createElement('th');
     sale.textContent = store.cookie_sales_per_hour[i];
     row.appendChild(sale);
     console.log(sale);
   }
   //creates the total
-  var total = document.createElement('th');
+  let total = document.createElement('th');
   total.textContent = store.cookie_sales_per_day;
   row.appendChild(total);
 
@@ -89,23 +89,22 @@ function customer_per_hour (min , max ) {
 }
 
 
-for (var i = 0; i < total_stores.length; i++) {
+for (let i = 0; i < total_stores.length; i++) {
   total_stores[i].create_cookie_sales_per_hour();
   total_stores[i].daily_total();
   render(total_stores[i]);
 
 }
 
+// creates new instances for cookiestand
 function works (event) {
   event.preventDefault();
-  var name = event.target.name.value;
-  var min = parseInt(event.target.min.value);
-  var max = parseInt(event.target.max.value);
-  var avg = parseInt(event.target.average.value);
-  console.log('it works');
-  var newstore = new Cookie_store(name,min,max,avg);
+  let name = event.target.name.value;
+  let min = parseInt(event.target.min.value);
+  let max = parseInt(event.target.max.value);
+  let avg = parseInt(event.target.average.value);
+  let newstore = new Cookie_store(name,min,max,avg);
 
-  console.log(newstore.cookie_sales_per_hour);
   newstore.create_cookie_sales_per_hour();
   newstore.daily_total();
 
@@ -118,38 +117,37 @@ function works (event) {
   form.reset();
 }
 
-var form = document.getElementById('form');
+let form = document.getElementById('form');
 form.addEventListener('submit', works);
 
-var tm = [];
-
+let tm = [];
 
 function footer () {
   /// creates the total name
-  var row = document.createElement('tr');
+  let row = document.createElement('tr');
   row.id = 'total-row';
 
-  var final = document.createElement('th');
+  let final = document.createElement('th');
   final.textContent = ('TOTAL');
   row.appendChild(final);
 
   // //creates the total sale of cookies for every hour
-  for (var j = 0; j < hours.length ; j++) {
-    var sum = 0;
-    for (var i = 0; i < total_stores.length; i++) {
+  for (let j = 0; j < hours.length ; j++) {
+    let sum = 0;
+    for (let i = 0; i < total_stores.length; i++) {
       sum = sum + total_stores[i].cookie_sales_per_hour[j];
     }
 
-    var total = document.createElement('th');
+    let total = document.createElement('th');
     total.textContent = sum;
     row.appendChild(total);
   }
   ///creates the daily totals.
-  var add = 0;
-  for (var i = 0; i < total_stores.length; i++) {
+  let add = 0;
+  for (let i = 0; i < total_stores.length; i++) {
     add = add+ total_stores[i].cookie_sales_per_day;
   }
-  var all = document.createElement('th');
+  let all = document.createElement('th');
   all.textContent = add;
   row.appendChild(all);
 
